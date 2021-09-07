a89ea70882deb5f16862f6d01255e79d74f575bf
Cojohnny97 <81743238+Cojohnny97@users.noreply.github.com>
2021-09-07 12:56:32 +0200

    fix

--- src/endpoints/ProjectStatuses.js
+++ src/endpoints/ProjectStatuses.js
@@ -2,20 +2,32 @@ import { ProjectStatus } from "../model/ProjectStatus"
 
 export class ProjectStatuses {
 
-    constructor (client, projectTemplateId ="") {
+    constructor (client, projectId, projectTemplateId ="") {
         this._client = client
+        this._projectId = projectId
         this._projectTemplateId = projectTemplateId
     }
 
 
-    async get (projectId) {
+    /**
+     * Returns all project statuses of a specified project.
+     * @param {String} projectId 
+     * @returns {Array<Promise<ProjectStatus>>}
+     */
+    async get (projectStatusId) {
         const list = await this.list()
         return list.filter(l => l.projectId === projectId)
     }
-    
+
+    /**
+     * Returns all project statuses.
+     * @returns {Array<Promise<ProjectStatus>>}
+     */
     async list () {
         const response = await this._client.get('/projectstatuses')
         const data = response.data()
-        return data.map(d => new ProjectStatus(d))
+        return data.map(d => new ProjectStatus(d)).filter(p => p.projectId === this._projectId)
     }
+
+    //async update
 }
