b88d4fe525446ad3dd1181428decf67b41853e7b
MurmeltierS <kevinsieger22@gmail.com>
2021-09-11 01:59:04 +0200

    remove temporary tests

--- tests/projects.test.js
+++ tests/projects.test.js
@@ -1,6 +1,10 @@
 import { Client } from '../src/client'
 import { Projects } from '../src/endpoints/Projects'
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3aWQiOiJhMjQ4NjgyMy05OTg2LWVhMTEtYTk0Yy0wMDE1NWQzMTRjNWEiLCJpaWQiOiI5YTM3NjVhNy0wMjBjLWVjMTEtYjU2My1kYzk4NDAyM2Q0N2UiLCJ1aWQiOiI5YTM3NjVhNy0wMjBjLWVjMTEtYjU2My1kYzk4NDAyM2Q0N2UiLCJuYW1lIjoiam9obm55LXRlc3QiLCJhenAiOiJqb2hubnktdGVzdCIsInNjb3BlIjoib2ZmbGluZV9hY2Nlc3MiLCJydGlkIjoiOWEzNzY1YTctMDIwYy1lYzExLWI1NjMtZGM5ODQwMjNkNDdlIiwibmJmIjoxNjMwNTk2NjE5LCJleHAiOjQ3NTQ3MzQyMTksImlzcyI6Imh0dHBzOi8vYXdvcmsuaW8vIiwiYXVkIjoiYXctYWNjb3VudHMifQ.hPPBLSFE9QZ32scMd_998c3EI8lsW-w0RRivK3RjRTI'
+import { API_KEY } from './credentials'
+
+/**
+ * @todo TODO: rewrite these tests to be real value
+ */
 
 test('1=1t', async () => {
     const client = new Client(API_KEY)
