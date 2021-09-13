724165fa304077e193bbffc8e8d2d52bd32da288
MurmeltierS <kevinsieger22@gmail.com>
2021-09-14 01:14:47 +0200

    a lot of weird configring

---
+++ src/client/index.d.ts
@@ -0,0 +1,31 @@
+/**
+ * Client class used to perform the http requests
+ * @category Util
+ * @class
+ */
+export class Client {
+    static getPlainObject(obj: any): any;
+    constructor(apiKey?: string, basePath?: string);
+    basePath: string;
+    http: import("axios").AxiosInstance;
+    /**
+     *
+     * @param {String} path
+     * @param {Object} params
+     * @returns {AworkResponse}
+     */
+    get(path: string, params?: any, headers?: {}): AworkResponse;
+    post(path: any, payload: any, params?: {}, headers?: {}): Promise<AworkResponse>;
+    put(path: any, payload: any, params?: {}, headers?: {}): Promise<AworkResponse>;
+    delete(path: any, params?: {}, headers?: {}): Promise<AworkResponse>;
+}
+declare class AworkResponse {
+    constructor(status: any, headers: any, data: any);
+    _status: any;
+    _headers: any;
+    _data: any;
+    get status(): any;
+    get headers(): any;
+    data(): any;
+}
+export {};
