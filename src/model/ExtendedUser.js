99ff1797eba17221f41eee2f58b5a5c51e969726
chiara-purger <89785676+chiara-purger@users.noreply.github.com>
2021-09-14 17:38:15 +0200

    Completed Users endpoint, UserContactInfos endpoint and the models User, ExtendedUser and UserContactInfo

---
+++ src/model/ExtendedUser.js
@@ -0,0 +1,55 @@
+import { User } from './User'
+
+
+/**
+ * User Model
+ * @category Models
+ * @extends User
+ * @class
+ */
+ export class ExtendedUser extends User {
+    /**
+     * @typedef {ExtendedUser & {_plain?: true}} ExtendedUserLike
+     */
+
+    /**
+     * Model constructor
+     * @param {ExtendedUserLike} data 
+     */
+    constructor(data) {
+        /** @private */
+        this._data = data || {}
+    }
+
+    /**
+     * The account id of the user. Only used internally.
+     * @type {String} uuid
+     */
+     get accountId () {
+        return this._data.accountId
+    }
+
+    /**
+     * @typedef {Object} TeamsObject
+     * @property {String} id The Id of the team.
+     * @property {String} name The name of the team.
+     * @property {String} icon The icon which is assigned to the team.
+     * @property {String} color The color of the team.
+     */
+
+    /**
+     * The teams the user is associated to.
+     * @type {Array<TeamsObject>}
+     */
+    get teams () {
+        return this._data.teams
+    }
+
+    toPlainObject () {
+        return {
+            ...super.toPlainObject(),
+            accountId: this._data.accountId,
+            teams: this._data.teams
+        }
+    }
+}
