3d4e1e9083073be17d573fcca3f089d6634ddb8b
MurmeltierS <kevinsieger22@gmail.com>
2021-09-20 12:58:36 +0200

    test typedef import hack

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import { Projects } from "./endpoints/Projects"
 import { Teams } from "./endpoints/Teams"
 
 /**
+ * @typedef {Object} FilterQuery
+ */
+
+
+/**
+ * @typedef {Object} ListOptions
+ * @property {Number} [page] The current page number
+ * @property {Number} [pageSize] How many items should be returned on each page. Maximum of 1000.
+ * @property {(FilterQuery|String)} [filterBy] Filter String or FilterQuery
+ */
+
+/**
+ * @typedef {('attach_money'|'poll'|'golf_course'|'all_inclusive'|'portrait'|'timeline'|'transform'|'description'|'folder'|'computer'|'web'|'phone_iphone'|'cloud'|'local_movies'|'shopping_cart'|'brush'|'image'|'camera_alt'|'movie_creation'|'public'|'whatshot'|'extension'|'explore'|'lock'|'settings'|'stars'|'store'|'school'|'local_bar'|'question_answer'|'favorite'|'work'|'flight_takeoff'|'map'|'local_dining')} Icon Possible awork icons.
+ */
+
+/**
+ * @typedef {('red'|'coral'|'yellow'|'green'|'teal'|'arctic'|'blue'|'azure'|'purple'|'violet')} Color Colors supported by awork.
+ */
+
+/**
  * Awork Class
  * @category Util
  */
